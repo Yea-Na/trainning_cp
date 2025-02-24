f1444b0f9b4e6d93b42a1ec6a714609256a6623d
AnJungGeon <gunn0128@gmail.com>
2025-02-24 12:02:53 +0900

    [feature] 배달 dto 작성

---
+++ src/main/java/com/training_delivery/demo/domain/delivery/dto/response/DeliveryCompleteResponseDto.java
@@ -0,0 +1,25 @@
+package com.server.delivery.domain.delivery.dto.res;
+
+import java.util.UUID;
+
+
+import com.training_delivery.demo.model.delivery.entity.Delivery;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeliveryCompleteResponseDto {
+
+	private OrderInfo orderInfo;
+	private UUID deliveryId;
+
+	public static DeliveryCompleteResponseDto from(Delivery delivery, Order order) {
+		return DeliveryCompleteResponseDto.builder()
+				.orderInfo(OrderInfo.from(order))
+				.deliveryId(delivery.getDeliveryUuid())
+				.build();
+	}
+}
---
+++ src/main/java/com/training_delivery/demo/domain/delivery/dto/response/DeliverySearchResponseDto.java
@@ -0,0 +1,37 @@
+package com.training_delivery.demo.domain.delivery.dto.response;
+
+
+import com.training_delivery.demo.model.delivery.entity.Delivery;
+import com.training_delivery.demo.model.delivery.entity.DeliveryStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeliverySearchResponseDto {
+
+	private UUID orderId;
+	private LocalDateTime orderCreatedAt;
+	private UUID deliveryId;
+	private String deliveryAddress;
+	private DeliveryStatus deliveryStatus;
+	private LocalDateTime deliveryStartTime;
+	private int estimatedDeliveryTime;
+
+	public static DeliverySearchResponseDto of(Order order, Delivery delivery) {
+		return DeliverySearchResponseDto.builder()
+				.orderId(order.getOrderUuid())
+				.orderCreatedAt(order.getCreatedAt())
+				.deliveryId(delivery.getDeliveryUuid())
+				.deliveryAddress(order.getDeliveryAddress())
+				.deliveryStatus(delivery.getStatus())
+				.deliveryStartTime(delivery.getDeliveryStartTime())
+				.estimatedDeliveryTime(delivery.getDeliveryArrivalTime())
+				.build();
+	}
+}
