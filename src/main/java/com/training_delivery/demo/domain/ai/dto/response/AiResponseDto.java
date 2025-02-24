46334f32ccf684fcfe94b9916378b149148fcb15
AnJungGeon <gunn0128@gmail.com>
2025-02-24 10:54:06 +0900

    [feature] ai dto 작성

--- src/main/java/com/training_delivery/demo/domain/ai/dto/response/AiResponseDto.java
+++ src/main/java/com/training_delivery/demo/domain/ai/dto/response/AiResponseDto.java
@@ -1,4 +1,10 @@
 package com.training_delivery.demo.domain.ai.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class AiResponseDto {
+    private String aiResponse;
 }
